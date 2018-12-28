ebcb3c47a440f8ad91855d937a931dad12b0d3e2
offwork <chomunist@gmail.com>
2018-12-28 19:18:28 +0300

    boilerplate client

---
+++ greet/greet_client/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/offwork/grpc-go/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+
+	cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
